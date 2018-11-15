6fb67e9582ad7c28423ed1805d40bf8aab6afab4
Isa Levine <isaacdlevine@gmail.com>
2018-11-15 19:42:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["cow",
+  "moo",
+  "spots",
+  "chocolate milk"
+  ]
 end
 
 def add_element_to_end_of_array(array, element)
